2ec5baaefa25be2b06b46a038213a16995e7b2cd
Piotr Gołaszewski <piogola@gmail.com>
2017-08-17 22:43:02 +0200

    Initial commit

---
+++ src/main/java/hellowworld/Greeter.java
@@ -0,0 +1,7 @@
+package hellowworld;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hellowworld/HellowWorld.java
@@ -0,0 +1,13 @@
+package hellowworld;
+
+import org.joda.time.LocalTime;
+
+public class HellowWorld {
+
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
